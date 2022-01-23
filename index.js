53bccdecf924d4eee5c48145e8f42f3a962f2ee2
adbenitez <asieldbenitez@gmail.com>
2022-01-22 20:20:43 -0500

    add random color to particles in the initial state

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function create() {
     emitter = particles.createEmitter({
         speed: 100,
         scale: { start: 1, end: 0 },
+        tint: {start: 0, end: 16777214, random: true},
         blendMode: 'ADD'
     });
 
